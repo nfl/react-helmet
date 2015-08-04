6b6fdca6434e0f79b63302650d4245c17836fc4a
Welch, Chris <Chris.Welch@NFL.com>
2015-08-03 19:09:16 -0700

    Importing a smaller subset of core-js modules instead of the whole core-js/shim

--- src/index.jsx
+++ src/index.jsx
@@ -1,4 +1,10 @@
-import "core-js/shim";
+import "core-js/fn/array/for-each";
+import "core-js/fn/array/from";
+import "core-js/fn/map";
+import "core-js/fn/object/get-own-property-symbols";
+import "core-js/fn/object/is";
+import "core-js/fn/object/keys";
+import "core-js/fn/set";
 import Helmet from "./Helmet";
 
 export default Helmet;
