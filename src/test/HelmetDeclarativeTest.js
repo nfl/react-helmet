993a52f87c2b2f2cd277b51736735cbfdf48292f
Welch, Chris <chris.welch@nfl.com>
2017-03-14 14:56:25 -0700

    fix lang warning

--- src/test/HelmetDeclarativeTest.js
+++ src/test/HelmetDeclarativeTest.js
@@ -1,5 +1,6 @@
 /* eslint max-nested-callbacks: [1, 5] */
 /* eslint-disable react/jsx-sort-props */
+/* eslint-disable jsx-a11y/html-has-lang */
 
 import React from "react";
 import ReactDOM from "react-dom";
