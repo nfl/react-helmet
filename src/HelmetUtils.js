6e6538881cc9bbef6b72a872f997571c917b88d7
Josh Gummersall <joshgummersall@users.noreply.github.com>
2018-02-26 11:31:15 -0800

    FIX: titleTemplate bug with multiple title children components (#351)
    Regexp replacement with a function will call .toString() on the
    replacement candidate. That would include extra commas in page titles
    where the <title> component had multiple children.

--- src/HelmetUtils.js
+++ src/HelmetUtils.js
@@ -33,7 +33,13 @@ const getTitleFromPropsList = propsList => {
 
     if (innermostTemplate && innermostTitle) {
         // use function arg to avoid need to escape $ characters
-        return innermostTemplate.replace(/%s/g, () => innermostTitle);
+        return innermostTemplate.replace(
+            /%s/g,
+            () =>
+                Array.isArray(innermostTitle)
+                    ? innermostTitle.join("")
+                    : innermostTitle
+        );
     }
 
     const innermostDefaultTitle = getInnermostProperty(
