e11bbac844c40e2bd65b939c12856b4f7c583d65
Saibamen <saibamenppl@gmail.com>
2019-10-26 22:48:39 +0000

    Increase timeout

--- karma.config.js
+++ karma.config.js
@@ -102,7 +102,7 @@ module.exports = function(config) {
         },
 
         // If browser does not capture in given timeout [ms], kill it
-        captureTimeout: 60000,
+        captureTimeout: 150000,
 
         // Continuous Integration mode
         // if true, it capture browsers, run tests and exit
