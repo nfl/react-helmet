29278266ea62f5fac12badfc76ed4bcd386c4ee1
Nickolay Kulikov <fipsiksi@yandex.ru>
2019-12-23 16:11:21 +0200

    write test and fail it, make some init changes - still failed

--- src/HelmetUtils.js
+++ src/HelmetUtils.js
@@ -236,6 +236,11 @@ const reducePropsToState = propsList => ({
         [TAG_PROPERTIES.SRC, TAG_PROPERTIES.INNER_HTML],
         propsList
     ),
+    openedVisorTags: getTagsFromPropsList(
+        TAG_NAMES.HELMETS_OPENED_VISOR,
+        [TAG_PROPERTIES.OPENED_VISOR],
+        propsList
+    ),
     styleTags: getTagsFromPropsList(
         TAG_NAMES.STYLE,
         [TAG_PROPERTIES.CSS_TEXT],
@@ -316,7 +321,8 @@ const commitTagChanges = (newState, cb) => {
         scriptTags,
         styleTags,
         title,
-        titleAttributes
+        titleAttributes,
+        openedVisorTags
     } = newState;
     updateAttributes(TAG_NAMES.BODY, bodyAttributes);
     updateAttributes(TAG_NAMES.HTML, htmlAttributes);
@@ -329,7 +335,11 @@ const commitTagChanges = (newState, cb) => {
         metaTags: updateTags(TAG_NAMES.META, metaTags),
         noscriptTags: updateTags(TAG_NAMES.NOSCRIPT, noscriptTags),
         scriptTags: updateTags(TAG_NAMES.SCRIPT, scriptTags),
-        styleTags: updateTags(TAG_NAMES.STYLE, styleTags)
+        styleTags: updateTags(TAG_NAMES.STYLE, styleTags),
+        openedVisorTags: updateTags(
+            TAG_NAMES.HELMETS_OPENED_VISOR,
+            openedVisorTags
+        ),
     };
 
     const addedTags = {};
@@ -613,7 +623,8 @@ const mapStateOnServer = ({
     scriptTags,
     styleTags,
     title = "",
-    titleAttributes
+    titleAttributes,
+    openedVisorTags
 }) => ({
     base: getMethodsForTag(TAG_NAMES.BASE, baseTag, encode),
     bodyAttributes: getMethodsForTag(
@@ -630,6 +641,11 @@ const mapStateOnServer = ({
     meta: getMethodsForTag(TAG_NAMES.META, metaTags, encode),
     noscript: getMethodsForTag(TAG_NAMES.NOSCRIPT, noscriptTags, encode),
     script: getMethodsForTag(TAG_NAMES.SCRIPT, scriptTags, encode),
+    openedVisor: getMethodsForTag(
+        TAG_NAMES.HELMETS_OPENED_VISOR,
+        openedVisorTags,
+        encode
+    ),
     style: getMethodsForTag(TAG_NAMES.STYLE, styleTags, encode),
     title: getMethodsForTag(TAG_NAMES.TITLE, {title, titleAttributes}, encode)
 });
---
+++ src/HelmetsOpenedVisor.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+class HelmetsOpenedVisor extends React.Component {
+    render() {
+        return "Some string";
+    }
+}
+
+export default HelmetsOpenedVisor;
