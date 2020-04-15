e584b74de208f24fee500c6ad30efd2cf86dcbc1
Richard Herrera <rich@doctyper.com>
2020-04-15 16:15:27 -0700

    chore: wip

--- example/index.tsx
+++ example/index.tsx
@@ -4,29 +4,45 @@ import * as ReactDOM from "react-dom";
 import { HelmetProvider, Helmet } from "../.";
 
 const App = () => {
+  const [toggle, setToggle] = React.useState(true);
+
+  React.useEffect(() => {
+    // @ts-ignore
+    [...document.head.children].forEach((f) => console.log(f));
+  });
+
   return (
     <div>
       <HelmetProvider>
         <Helmet>
           <html lang="ja" id="html-tag" title="html tag" />
           <title>Test Title</title>
-          <meta charSet="utf-8" />
           <meta name="description" content="Test description" />
         </Helmet>
 
-        <div>
-          <Helmet>
-            <title>Child One Title</title>
-            <meta name="description" content="Inner description" />
-            <meta name="keywords" content="test,meta,tags" />
-          </Helmet>
-
+        {toggle ? (
           <div>
             <Helmet>
-              <title>Child Two Title</title>
+              <title>Child One Title</title>
+              <meta name="description" content="Inner description" />
+              <meta name="keywords" content="test,meta,tags" />
+            </Helmet>
+
+            <div>
+              <Helmet>
+                <title>{`Toggle 1: ${toggle}`}</title>
+              </Helmet>
+            </div>
+          </div>
+        ) : (
+          <div>
+            <Helmet>
+              <title>{`Toggle 2: ${!toggle}`}</title>
             </Helmet>
           </div>
-        </div>
+        )}
+
+        <button onClick={() => setToggle((p) => !p)}>Test</button>
       </HelmetProvider>
     </div>
   );
